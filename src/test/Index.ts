824f6a1ecbe38e6507ae98e22468054230f9f5b4
Yosua Kristianto <yosuakristianto754@gmail.com>
2022-07-11 15:14:00 +0700

    Completing writing test

--- src/test/Index.ts
+++ src/test/Index.ts
@@ -1,11 +1,5 @@
 // Make sure to put any pre-condition to make sure your application is running well in here
-import dotenv from 'dotenv';
-dotenv.config();
-
-import config from '../main/config/Config';
-import { Log } from "../main/config/Logging";
-import NoSQLConfig from '../main/config/NoSQLConfig';
-
+import '../main/config/NoSQLConfig';
 
 /**
  * WRITING TEST
@@ -13,13 +7,9 @@ import NoSQLConfig from '../main/config/NoSQLConfig';
  * You can specify every unit Tests in here.
  */
 async function main() {
-    NoSQLConfig;
 
     await require("./example/Index");
-    const mqttSubscriberTesting = require("./example/mqttSubscriberTesting");
-    await mqttSubscriberTesting;
+    await require("./example/mqttSubscriberTesting");
 }
 
-main().then(() => {
-    process.exit(0);
-})
+main().then(() => { console.log("Testing Done. Use CTRL+C or COMMAND+C to close this test run.") });
--- src/test/example/mqttSubscriberTesting.ts
+++ src/test/example/mqttSubscriberTesting.ts
@@ -1,7 +1,6 @@
-import mongoose from "mongoose";"mongoose"
 import { MQTTSubscriberHandler } from "../../main/handler/MQTTSubscriberHandler";
 
-export default async function main(): Promise<void> {
+async function main(): Promise<void> {
     console.log("Begin running MQTTSubscriberTesting.ts");
 
     const handler = new MQTTSubscriberHandler();
@@ -10,11 +9,11 @@ export default async function main(): Promise<void> {
 
     try {
         handler.handleActivityLog({"feature_key": 'Ini adalah feature_key', "user_id": '12345678910', "description": 'Ini adalah description', "action": 'Ini adalah action'});
-        console.log("MongoDB Connection State: "+ mongoose.connection.readyState);
         console.log(`Handle Activity Log Function Test Passed with time of ${(new Date().getTime() - timeInit.getTime()) / 1000} seconds! `);
     } catch (error: any) {
         console.log("Handle Activity Log Function Error: "+ error.message);
     }  
 
-  }
-  
+}
+
+export default main();
