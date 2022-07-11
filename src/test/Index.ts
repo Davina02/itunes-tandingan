9ae3ed0c5da114b00dadb5ddfbd70dbcf0eac49e
Davina02 <72156781+Davina02@users.noreply.github.com>
2022-07-11 23:50:45 +0700

    Update writing test

--- src/test/Index.ts
+++ src/test/Index.ts
@@ -4,9 +4,12 @@
  * You can specify every unit Tests in here.
  */
 
-import "../main/config/NoSQLConfig";
-setTimeout(function(){}, 2000);
-import "./example/Index";
-import "./example/mqttSubscriberTesting";
+ import "../main/config/NoSQLConfig";
 
-process.exit(0);
+ async function main() {
+     await require("./example/Index");
+     await require("./example/mqttSubscriberTesting");
+ }
+ 
+ // process.exit(0);
+ main().then(() => { console.log("Testing Done. Use CTRL+C or COMMAND+C to close this test run.") });
